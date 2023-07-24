d75c7d89db33bf5c70262713a36b60d2b490a275
bogdan1775 <120658401+bogdan1775@users.noreply.github.com>
2023-07-24 17:42:50 +0300

    Create vma.h

---
+++ vma.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <inttypes.h>
+#include <stddef.h> 
+
+/* TODO : add your implementation for doubly-linked list */
+/*typedef struct { 
+    size_t data_size;
+    void *next;
+    void *prev;
+    void *data;
+
+} list_t;*/
+
+typedef struct dll_node_t dll_node_t;
+struct dll_node_t
+{
+    void* data; 
+    dll_node_t *prev, *next;
+};
+
+typedef struct list_t list_t;
+struct list_t
+{
+    dll_node_t* head;
+    size_t data_size;
+    size_t size;
+    
+}; 
+
+typedef struct {
+   uint64_t start_address; 
+   size_t size; 
+   list_t* miniblock_list;
+} block_t;
+
+typedef struct {
+   uint64_t start_address;
+   size_t size;
+   uint8_t perm;
+   void* rw_buffer;
+} miniblock_t;
+
+typedef struct {
+   uint64_t arena_size;
+   list_t *alloc_list;
+} arena_t;
+
+arena_t* alloc_arena(const uint64_t size);
+void dealloc_arena(arena_t* arena);
+
+void alloc_block(arena_t* arena, const uint64_t address, const uint64_t size);
+void free_block(arena_t* arena, const uint64_t address);
+
+void read(arena_t* arena, uint64_t address, uint64_t size);
+void write(arena_t* arena, const uint64_t address,  const uint64_t size, int8_t *data);
+void pmap(const arena_t* arena);
+void mprotect(arena_t* arena, uint64_t address, int8_t *permission);
